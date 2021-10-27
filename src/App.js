00267b0a7926661f18751e0455e983e0a23b09c0
Gabe <gabenedden@gmail.com>
2021-10-27 17:12:01 -0400

    Add React Router and Global-Styles/Styled-Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+
+import Home from './pages/Home';
+import Destination from './pages/Destination';
+import Crew from './pages/Crew';
+import Technology from './pages/Technology';
 
 function App() {
   return (
-      <div>Start</div>
+      <Router>
+          <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/destination' component={Destination}/>
+                <Route exact path='/crew' component={Crew}/>
+                <Route exact path='/technology' component={Technology}/>
+          </Switch>
+      </Router>
   );
-}
+};
 
 export default App;
